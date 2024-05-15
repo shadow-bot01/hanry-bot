40e983668ad3b0083563cae6a9560d922352e6aa
shadow-bot01 <odrmod@gmail.com>
2024-05-15 13:40:19 +0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -501,7 +501,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = ['اوامر'] 
+handler.command = ['المهام'] 
 
 export default handler
 function clockString(ms) {
